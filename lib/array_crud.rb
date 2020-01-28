4e1a22cd5068137664da84dec15055a61fc34f7c
Hailey Bongo <haileyebongo@gmail.com>
2020-01-28 03:00:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   my_array = ["one"]
-  my_array << "two"
-  my_array << "three"
+  my_array.push ("two")
 end
 
 def add_element_to_start_of_array(array, element)
